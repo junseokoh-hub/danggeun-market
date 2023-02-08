a1a9d280143e68dfa459256b14c83353caa50c6f
junseokoh-hub <lunulatecat@naver.com>
2023-02-08 16:32:28 +0900

    upload api

---
+++ pages/api/products/index.tsx
@@ -0,0 +1,38 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) => {
+  const { name, price, description } = req.body;
+  const { user } = req.session;
+
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "xx",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  res.json({
+    ok: true,
+    product,
+  });
+};
+
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  }),
+);
