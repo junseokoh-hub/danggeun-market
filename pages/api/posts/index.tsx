08968eebd4e9648842cdeb03539cff0a5021cbba
junseokoh-hub <lunulatecat@naver.com>
2023-02-08 20:12:04 +0900

    forms and handlers

---
+++ pages/api/posts/index.tsx
@@ -0,0 +1,32 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+) => {
+  const { question } = req.body;
+  const { user } = req.session;
+
+  const post = await client.post.create({
+    data: {
+      question,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  res.json({ ok: true, post });
+};
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  }),
+);
